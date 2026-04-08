6cfbe0386f42482a0919882bc50543a978bde157
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-04-08 23:27:42 +0500

    routes setting

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className=" bg-amber-500">Home Route</div>;
+  return <div className=" bg-blue-600">Contacts Route</div>;
 };
 
 export default Home;
