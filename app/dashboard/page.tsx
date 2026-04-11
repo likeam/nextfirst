9a4a08203b0e280a9d3fc0ec10777939d7401842
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-04-11 17:11:14 +0500

    Buliding Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,10 +14,10 @@ const Dashboard = () => {
             your progress.
           </p>
         </div>
-        <div className="">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           <Link
             href="/dashboard/users/users-list"
-            className=" inline-flex items-center justify-center bg-blue-600 text-white px-8 py-3 rounded-lg font-semibold hover:bg-blue-700 transition-colors shadow-lg hover:shadow-xl"
+            className=" bg-blue-100 hover:bg-blue-200 p-6 rounded-xl shadow-md hover:shadow-xl transition-all duration-200 ease-in-out transform hover:scale-110"
           >
             <div className="">
               <h2 className="">Tasks</h2>
