73a69e7e2c4f5b95588d1ce566e9b1c07ed19856
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-04-15 23:14:25 +0500

    Building Analytics Section

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -33,6 +33,12 @@ const Analytics = () => {
         <h1 className=" text-3xl font-bold text-gray-800 mb-8">
           Analytics Dashboard
         </h1>
+
+        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+          {metrics.map((metric, index) => (
+            <div className=""></div>
+          ))}
+        </div>
       </div>
     </div>
   );
