07dc6c44ad28754a973efdc68018ced1dc8ae884
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-04-18 23:15:01 +0500

    Builidng Profile Settings

--- app/dashboard/settings/profile/page.tsx
+++ app/dashboard/settings/profile/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ProfileSettings = () => {
-  return <div>ProfileSettings</div>;
+  return (
+    <div className=" min-h-screen bg-gradient-to-br from-blue-300 to-blue-950 p-8">
+      <div className=" max-w-4xl mx-auto bg-gray-300 rounded-lg shadow-2xl p-8">
+        <h1 className=" text-3xl font-bold text-gray-900 mb-4">
+          Profile Settings
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileSettings;
