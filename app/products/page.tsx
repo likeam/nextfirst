928f94116a76c9530fe8c1ec282f3d6d0ce66cc1
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2026-04-20 22:14:58 +0500

    Creating Products

---
+++ app/products/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <p className="">
+        {" "}
+        You can view the products by going to /products/allproducts
+      </p>
+      <p className="">
+        {" "}
+        You can view a specific product by going to /products/specificproduct
+      </p>
+    </div>
+  );
+};
+
+export default Products;
